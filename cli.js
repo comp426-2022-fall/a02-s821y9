3dba73e5fb1a4747f4c297117c734c531b9ce2bb
s821y9 <73866445+s821y9@users.noreply.github.com>
2022-11-28 13:22:19 -0500

    Update cli.js

--- cli.js
+++ cli.js
@@ -1,87 +1,69 @@
 #!/usr/bin/env node
 
+import minimist from "minimist";
+import nodefetch from "node-fetch";
+import moment from "moment-timezone";
 
-import moment from 'moment-timezone'
-import minimist from 'minimist'
-import fetch from 'node-fetch'
+const args = minimist(process.argv.slice(2));
+let latitude;
+let longitude;
+
+if(args.h){
+  	console.log(`Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE
+    -h            Show this help message and exit.
+    -n, -s        Latitude: N positive; S negative.
+    -e, -w        Longitude: E positive; W negative.
+    -z            Time zone: uses tz.guess() from moment-timezone by default.
+    -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.
+    -j            Echo pretty JSON from open-meteo API and exit.
+    `);
+}else{
+	let latitude = '0';
+	if(args.n){
+		latitude = args.n;
+	}
+
+	if(args.s){
+		latitude = args.s;
+	}else{
+		console.log("Latitude must be in range");
+	}
+
+	let longtitude = '0';
+	if(args.e){
+		longtitude = args.e;
+	}
+
+	if(args.w){
+    		longtitude = args.w;
+	}else{
+		console.log("Longtitude must be in range")
+	}
+
+	var timezone = moment.tz.guess();
+	if(args.z){
+    		timezone = args.z;
+	}
 
 
+	
 
+	const response = await nodefetch('https://api.open-meteo.com/v1/forecast?latitude=' + latitude + '&longitude=' + longtitude + '&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,precipitation_sum&timezone=' + timezone);
 
-const args = minimist(process.argv.slice(2))
-const timezone = moment.tz.guess()
+	const data = await response.json();
 
+	if (args.j) {
+		console.log(data);
+		process.exit(0);
+	}	 
 
-if (args.h){
-	try{
-		console.log(`
-		Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE
-		    -h            Show this help message and exit.
-		    -n, -s        Latitude: N positive; S negative.
-		    -e, -w        Longitude: E positive; W negative.
-		    -z            Time zone: uses tz.guess() from moment-timezone by default.
-		    -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.
-		    -j            Echo pretty JSON from open-meteo API and exit.
-		`)
-		process.exitCode = 0;
-	} catch (err) { 
-	process.exitCode = 1;
-	 }
+	const days = args.d
+
+	if (days == 0) {
+		console.log("today.")
+	} else if (days > 1) {
+		console.log("in " + days + " days.")
+	} else {
+  		console.log("tomorrow.")
+	}
 }
-
-
-
-if (args.z) {
-    timezone = args.z
-}
-
-let latitude = '0'
-if (args.n) {
-    latitude = args.n
-} else if (args.s) {
-    latitude = args.s
-} else {
-    console.log("Latitude must be in range")
-}
-
-let longitude = '0'
-if (args.e) {
-    longitude = args.e
-} else if (args.w) {
-    longitude = args.w
-} else {
-    console.log("Longitude must be in range")
-}
-
-
-
-// Make a request
-const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude="+latitude+"&longitude="+longitude+"&daily=precipitation_hours&timezone=" + timezone');
-
-
-// Get the data from the request
-const data = await response.json();
-
-
-
-
-
-if (args.j) {
-    console.log(data);
-    process.exit(0);
-}
-
-const days = args.d
-if (days == 0) {
-    console.log("today.")
-} else if (days > 1) {
-    console.log("in " + days + " days.")
-} else {
-    console.log("tomorrow.")
-}
-
-
-
-
-
-
